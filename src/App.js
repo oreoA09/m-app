13b637831e793a0b1b63d7bcbf8d8b7a08e86aca
oreoA09 <oreo.akanni@gmail.com>
2022-11-05 18:55:14 +0000

    implement fetch use Effect in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Gallery from './components/Gallery';
+import Searchbar from './components/Searchbar';
 import './App.css';
 
 function App() {
+  let [search, setSearch] = useState("")
+  let [message, setMessage] = useState('Search for Songs')
+  let [data, setData] = useState([])
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      document.title = `${search} Songs`
+      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+      const resData = await response.json()
+      // console.log(resData)
+      if(resData.results.length > 0){
+        setData(resData.results)
+      }else {
+        setMessage('Content Not Found')
+      }
+    }
+    fetchData()
+  }, [search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Searchbar />
+      {message}
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/Galleryitem.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function GalleryItem(props){
+    let [view, setView] = useState(false)
+
+    return(
+        <div onClick={}>
+            <p></p>
+        </div>
+    )
+}
+
+export default GalleryItem
