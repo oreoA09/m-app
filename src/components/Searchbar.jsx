2bc32c4dafdf5ba90b2e3c3282e08aa1d2240da1
oreoA09 <oreo.akanni@gmail.com>
2022-11-05 19:22:05 +0000

    trigger props in Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBar(props){
+    let [searchItem, setSearchItem] = useState("")
 
+    return(
+        <form onSubmit={(e) => props.handleSearch(e, searchItem)}>
+            <input type="text" placeholder= "Search songs" onChange={(e)=> setSearchItem(e.target.value)}/>
+            <input type="submit"/>
+        </form>
+    )
 }
 
 export default SearchBar
