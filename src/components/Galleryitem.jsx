7c756f43b376990bf191aabce3d2b6ca709b378e
oreoA09 <oreo.akanni@gmail.com>
2022-11-05 19:51:55 +0000

    update Galleryitem.jsx

--- src/components/Galleryitem.jsx
+++ src/components/Galleryitem.jsx
@@ -3,9 +3,26 @@ import { useState } from "react";
 function GalleryItem(props){
     let [view, setView] = useState(false)
 
+    const simpleView = ()=>{
+        return(
+            <div>
+                <h2> {props.item.trackName}</h2>
+            </div>
+        )
+    }
+
+    const defaultView = ()=>{
+        return(
+            <div>
+                <h2> {props.item.trackName}</h2>
+            </div>
+        )
+    }
+
     return(
-        <div onClick={}>
-            <p></p>
+        <div onClick={()=> setView(!view)} style={{'display': 'inline-block'}}>
+            
+            {view ? defaultView() : simpleView()}
         </div>
     )
 }
